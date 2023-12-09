84641262bdd60ff130633201271a3da686956a1d
Akito Koga <akito121215@gmail.com>
2023-12-09 14:51:07 +0900

    add: neovimの実行手順を記載したドキュメントを追加

--- iterm/config.fish
+++ iterm/config.fish
@@ -1,7 +1,9 @@
+# mynvimを置く場所
+add_fish_path ~/.local/bin/
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
     eval (/opt/homebrew/bin/brew shellenv)
 end
 
 neofetch
-
---
+++ neovim/README.md
@@ -0,0 +1,15 @@
+# Neovim configuration
+
+## 手順
+
+1. Docker コンテナをビルドする
+
+```sh
+docker build -t mynvim .
+```
+
+2. mynvim を `~/.local/bin/` にコピーする
+
+```sh
+cp mynvim ~/.local/bin/
+```
