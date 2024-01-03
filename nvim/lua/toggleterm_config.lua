9c9707bf4ace9be941e08641918f58567796ad4b
GinKuReNai <akito121215@gmail.com>
2024-01-03 21:56:02 +0900

    add: <leader>ld で lazydocker が起動するように更新

--- nvim/lua/toggleterm_config.lua
+++ nvim/lua/toggleterm_config.lua
@@ -4,9 +4,19 @@ local lazygit = Terminal:new({
 	direction = "float",
 	hidden = true
 })
+local lazydocker = Terminal:new({
+    cmd = "lazydocker",
+    direction = "float",
+    hidden = true
+})
 
 function _lazygit_toggle()
 	lazygit:toggle()
 end
 
+function _lazydocker_toggle()
+    lazydocker:toggle()
+end
+
 vim.api.nvim_set_keymap("n", "<leader>lg", "<cmd>lua _lazygit_toggle()<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>ld", "<cmd>lua _lazydocker_toggle()<CR>", { noremap = true, silent = true })
