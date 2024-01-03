96f1698a4c4ca571b11cf2d6cbe21bce9d44161e
GinKuReNai <akito121215@gmail.com>
2024-01-03 21:56:34 +0900

    update: 日本語入力時でも入力モードが切り替わるように変更

--- nvim/lua/keymappings.lua
+++ nvim/lua/keymappings.lua
@@ -41,3 +41,24 @@ vim.keymap.set('n', '<A-h>', '<C-w>h', {})
 vim.keymap.set('n', '<A-j>', '<C-w>j', {})
 vim.keymap.set('n', '<A-k>', '<C-w>k', {})
 vim.keymap.set('n', '<A-l>', '<C-w>l', {})
+
+-- 'jj' でエスケープ（インサートモードからノーマルモードへ）
+vim.api.nvim_set_keymap('i', 'jj', '<ESC>', {noremap = true, silent = true})
+
+-- 'っj' でエスケープ（日本語入力中にも対応）
+vim.api.nvim_set_keymap('i', 'っj', '<ESC>', {noremap = true, silent = true})
+
+-- 日本語入力がオンのままでも使えるコマンド
+vim.api.nvim_set_keymap('n', 'あ', 'a', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', 'い', 'i', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', 'う', 'u', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', 'お', 'o', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', 'っd', 'dd', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', 'っy', 'yy', {noremap = true, silent = true})
+
+-- 入力モードでのカーソル移動
+vim.api.nvim_set_keymap('i', '<C-j>', '<Down>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('i', '<C-k>', '<Up>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('i', '<C-h>', '<Left>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('i', '<C-l>', '<Right>', {noremap = true, silent = true})
+
