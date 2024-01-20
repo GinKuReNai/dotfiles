5e6523b36e56e6ec08cf0bf780ff7ebd33c39ea4
GinKuReNai <akito121215@gmail.com>
2024-01-20 18:44:23 +0900

    update: bufferlineとlualineの設定を更新

--- nvim/lua/bufferline_config.lua
+++ nvim/lua/bufferline_config.lua
@@ -19,20 +19,21 @@ require('bufferline').setup{
 }
 
 -- bufferlineのキーマッピング
+local opts = { noremap = true, silent = true }
 -- 現在のバッファの左側のバッファを閉じる
-vim.keymap.set('n', '<leader>wl', '<CMD>BufferLineCloseRight<CR>')
+vim.keymap.set('n', '<leader>wl', '<CMD>BufferLineCloseRight<CR>', opts)
 -- 現在のバッファの右側のバッファを閉じる
-vim.keymap.set('n', '<leader>wh', '<CMD>BufferLineCloseLeft<CR>')
+vim.keymap.set('n', '<leader>wh', '<CMD>BufferLineCloseLeft<CR>', opts)
 -- 編集中のファイル以外のバッファを閉じる
-vim.keymap.set('n', '<leader>wall', '<CMD>BufferLineCloseOthers<CR>')
+vim.keymap.set('n', '<leader>wall', '<CMD>BufferLineCloseOthers<CR>', opts)
 -- 削除するファイルの選択モードに入る
-vim.keymap.set('n', '<leader>we', '<CMD>BufferLinePickClose<CR>')
+vim.keymap.set('n', '<leader>we', '<CMD>BufferLinePickClose<CR>', opts)
 
 -- ファイルをピン留めする
-vim.keymap.set('n', '<leader>wp', '<CMD>BufferLineTogglePin<CR>')
+vim.keymap.set('n', '<leader>wp', '<CMD>BufferLineTogglePin<CR>', opts)
 -- 左のファイルに移動
-vim.keymap.set('n', 'bl', '<CMD>BufferLineCycleNext<CR>')
+vim.keymap.set('n', '<S-l>', '<CMD>BufferLineCycleNext<CR>', opts)
 -- 右のファイルに移動
-vim.keymap.set('n', 'bh', '<CMD>BufferLineCyclePrev<CR>')
+vim.keymap.set('n', '<S-h>', '<CMD>BufferLineCyclePrev<CR>', opts)
 -- バッファをソート
-vim.keymap.set('n', '<leader>ws', '<CMD>BufferLineSortByDirectory<CR>')
+vim.keymap.set('n', '<leader>ws', '<CMD>BufferLineSortByDirectory<CR>', opts)
--- nvim/lua/lualine_config.lua
+++ nvim/lua/lualine_config.lua
@@ -43,21 +43,58 @@ require('lualine').setup {
         }
     },
     -- ファイル名は相対パスで表示
-    lualine_c = {{'filename', path = 1}},
+    lualine_c = {},
     lualine_x = {'encoding', 'fileformat', 'filetype'},
     lualine_y = {'progress'},
     lualine_z = {'location'}
   },
   inactive_sections = {
     lualine_a = {},
-    lualine_b = {},
-    lualine_c = {'filename'},
-    lualine_x = {'location'},
+    lualine_b = {'branch', 'diff', {
+            'diagnostics',
+            -- 対象のプラグイン
+            sources = { 'coc' },
+            -- 表示するエラーのレベル
+            sections = { 'error', 'warn', 'info', 'hint' },
+            -- エラーのレベルごとのアイコン
+            symbols = { error = ' ', warn = ' ', info = ' ', hint = ' ' },
+            -- エラーのレベルごとの色
+            diagnostics_color = {
+                error = 'DiagnosticError',
+                warn = 'DiagnosticWarn',
+                info = 'DiagnosticInfo',
+                hint = 'DiagnosticHint',
+            },
+            -- エラーのレベルごとの色を背景色にするかどうか
+            colored = true,
+            -- エラーのレベルごとの色を変更するかどうか
+            update_in_insert = false,
+            -- エラーのレベルごとの色を常に表示するかどうか
+            always_visible = false,
+        },
+    },
+    lualine_c = {},
+    lualine_x = {},
     lualine_y = {},
     lualine_z = {}
   },
   tabline = {},
-  winbar = {},
-  inactive_winbar = {},
+  -- 画面上部にファイルの相対パスを表示
+  winbar = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = {{'filename', path = 1}},
+    lualine_x = {'location'},
+    lualine_y = {},
+    lualine_z = {}
+  },
+  inactive_winbar = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = {{'filename', path = 1}},
+    lualine_x = {'location'},
+    lualine_y = {},
+    lualine_z = {}
+  },
   extensions = {}
 }
