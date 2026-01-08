152f0ae4731c68c801097d9c401c155ac116e7c6
GinKuReNai <akito121215@gmail.com>
2026-01-08 20:15:17 +0900

    delete :fire: fishの削除

--- fish/fisher.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# fishのパッケージマネージャ `fisher` のインストール
-curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish
-
-# fishのテーマとして bobthefish を追加
-fisher install oh-my-fish/theme-bobthefish
