05b14480bdbea64d95785ebc28a1460427e2682b
GinKuReNai <akito121215@gmail.com>
2026-02-05 12:05:53 +0900

    update :arrow_up: trouble.nvim のキーバインドの説明を日本語で詳しく

--- nvim/lua/plugins/trouble.lua
+++ nvim/lua/plugins/trouble.lua
@@ -2,12 +2,11 @@ return {
 	"folke/trouble.nvim",
 	opts = {},
 	keys = {
-		{ "<leader>xx", "<cmd>Trouble diagnostics toggle<cr>", desc = "Diagnostics (Trouble)" },
-		{ "<leader>xX", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>", desc = "Buffer Diagnostics (Trouble)" },
-        -- TODO: description を日本語で書く
-		{ "<leader>cs", "<cmd>Trouble symbols toggle focus=false<cr>" },
-		{ "<leader>cl", "<cmd>Trouble lsp toggle focus=false win.position=right<cr>" },
-		{ "<leader>xL", "<cmd>Trouble loclist toggle<cr>" },
-		{ "<leader>xQ", "<cmd>Trouble qflist toggle<cr>" },
+		{ "<leader>xx", "<cmd>Trouble diagnostics toggle<cr>", desc = "プロジェクト全体の警告やエラーを一覧表示" },
+		{ "<leader>xX", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>", desc = "現在開いているファイル（バッファ）内のエラーのみを表示" },
+		{ "<leader>cs", "<cmd>Trouble symbols toggle focus=false<cr>", desc = "関数名や変数名の一覧を表示し、コード内をジャンプしやすく" },
+		{ "<leader>cl", "<cmd>Trouble lsp toggle focus=false win.position=right<cr>", desc = "LSP定義/参照 定義元や参照先を右側にリスト表示" },
+		{ "<leader>xL", "<cmd>Trouble loclist toggle<cr>", desc = "Vim標準のロケーションリストをTroubleのUIで開く" },
+		{ "<leader>xQ", "<cmd>Trouble qflist toggle<cr>", desc = "Vim標準のクイックフィックスリストをTroubleのUIで開く" },
 	},
 }
