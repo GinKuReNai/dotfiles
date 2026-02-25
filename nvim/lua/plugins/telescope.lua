a32c2963f4b6e6a8a67571942ccd768b8413d226
GinKuReNai <akito121215@gmail.com>
2026-02-25 09:43:04 +0900

    refactor :recycle: telescopeの設定を分かりやすく

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,20 +1,48 @@
 return {
 	"nvim-telescope/telescope.nvim",
-	tag = "0.1.5",
-	dependencies = { "nvim-lua/plenary.nvim" },
-	config = function()
-        -- trouble.nvim との連携
-        local trouble = require("trouble.sources.telescope")
+	branch = "0.1.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-telescope/telescope-file-browser.nvim",
+		"nvim-telescope/telescope-frecency.nvim",
+	},
+	-- コマンド経由での呼び出し時にもプラグインをロードさせる
+	cmd = "Telescope",
 
-		-- Telescopeの設定
-		require("telescope").setup({
+	keys = {
+		{ "<leader>ff", "<cmd>Telescope find_files<CR>", desc = "Telescope: ファイル検索" },
+		{ "<leader>fg", "<cmd>Telescope live_grep<CR>", desc = "Telescope: テキスト検索" },
+		{ "<leader>gs", "<cmd>Telescope git_status<CR>", desc = "Telescope: Gitの変更ファイルを一覧表示" },
+		{ "<leader>gl", "<cmd>Telescope git_commits<CR>", desc = "Telescope: Gitのログ一覧を表示" },
+		{ "<leader>fo", "<cmd>Telescope oldfiles<CR>", desc = "Telescope: 編集履歴を一覧表示" },
+		{ "<leader>fb", "<cmd>Telescope buffers<CR>", desc = "Telescope: バッファーの一覧表示" },
+		{ "<leader>fh", "<cmd>Telescope help_tags<CR>", desc = "Telescope: ヘルプタグの検索" },
+		{ "<leader>fc", "<cmd>Telescope colorscheme<CR>", desc = "Telescope: カラーテーマの一覧表示" },
+		{ "<leader>fv", "<cmd>Telescope vim_options<CR>", desc = "Telescope: Vimオプションの表示" },
+		{ "<leader>fk", "<cmd>Telescope keymaps<CR>", desc = "Telescope: キーマップの一覧表示" },
+		{ "<leader>fn", "<cmd>Telescope notify<CR>", desc = "Telescope: 通知の一覧" },
+		{
+			"<leader>fbb",
+			"<cmd>Telescope file_browser path=%:p:h select_buffer=true<CR>",
+			desc = "Telescope: ツリー形式でファイルを検索",
+		},
+		{
+			"<leader>ffr",
+			"<cmd>Telescope frecency workspace=CWD<CR>",
+			desc = "Telescope: よく使用するファイルを表示",
+		},
+	},
+
+	opts = function()
+		local trouble = require("trouble.sources.telescope")
+
+		return {
 			defaults = {
-				-- 検索から除外するファイルやディレクトリ
 				file_ignore_patterns = {
-					"^.git/",
-					"^.cache/",
-					"^.vscode/",
-					"^.DS_Store",
+					"^%.git/",
+					"^%.cache/",
+					"^%.vscode/",
+					"^%.DS_Store",
 					"^Library/",
 					"^node_modules/",
 					"^vendor/",
@@ -23,101 +51,52 @@ return {
 					"Music",
 					"Pictures",
 				},
-				-- ripgrepのオプション
 				vimgrep_arguments = {
-					"rg", -- ripgrepコマンド
-					"--color=never", -- 色付けしない
-					"--no-heading", -- ファイル名を表示しない
-					"--with-filename", -- 検索結果にファイル名を含める
-					"--line-number", -- 検索結果に行番号を含める
-					"--column", -- 検索結果に列番号を含める
-					"--smart-case", -- 大文字小文字を区別しない（大文字が含まれていない場合）
-					"-uu", -- 隠しファイルも検索対象にする
-
-                    -- ripgrep で特定のディレクトリを検索対象から除外する
-                    '--glob=!**/.git/*',
-                    '--glob=!**/node_modules/*',
-                    '--glob=!**/vendor/*',
-                    '--glob=!**/Library/*',
-                    '--glob=!**/.terraform/*',
-                    '--glob=!**/.venv/*',
-                    '--glob=!**/__pycache__/*',
-                    '--glob=!**/.pytest_cache/*',
-                    '--glob=!**/.mypy_cache/*',
-                    '--glob=!**/package-lock.json',
-                    '--glob=!**/yarn.lock',
-                    '--glob=!**/*.svg',
-                    '--glob=!**/*.png',
-                    '--glob=!**/*.jpg',
-                    '--glob=!**/*.jpeg',
+					"rg",
+					"--color=never",
+					"--no-heading",
+					"--with-filename",
+					"--line-number",
+					"--column",
+					"--smart-case",
+					"-uu",
+					"--glob=!**/.git/*",
+					"--glob=!**/node_modules/*",
+					"--glob=!**/vendor/*",
+					"--glob=!**/Library/*",
+					"--glob=!**/.terraform/*",
+					"--glob=!**/.venv/*",
+					"--glob=!**/__pycache__/*",
+					"--glob=!**/.pytest_cache/*",
+					"--glob=!**/.mypy_cache/*",
+					"--glob=!**/package-lock.json",
+					"--glob=!**/yarn.lock",
+					"--glob=!**/*.svg",
+					"--glob=!**/*.png",
+					"--glob=!**/*.jpg",
+					"--glob=!**/*.jpeg",
 				},
-                mappings = {
-                    -- memo: 同一名のファイルの中身を詳しく閲覧したい場合等で, trouble.nvim の大きいウィンドウで中身を確認しながらファイル探索ができる
-                    i = { ["<c-t>"] = trouble.open}, -- Insert mode で Ctrl + t を押下すると Trouble で開く
-                    n = { ["<c-t>"] = trouble.open}, -- Normal mode でも同様
-                }
+				mappings = {
+					i = { ["<c-t>"] = trouble.open },
+					n = { ["<c-t>"] = trouble.open },
+				},
 			},
 			pickers = {
 				find_files = {
-					hidden = true, -- 隠しファイルを表示
+					hidden = true,
 				},
 			},
-		})
+		}
+	end,
 
-		-- Telescopeのキーマッピング
-		local builtin = require("telescope.builtin")
+	config = function(_, opts)
+		require("telescope").setup(opts)
 
-		-- ファイル検索
-		vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope: ファイル検索" })
-		-- テキスト検索
-		vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope: テキスト検索" })
-		-- Gitの変更ファイルを一覧表示
-		vim.keymap.set(
-			"n",
-			"<leader>gs",
-			builtin.git_status,
-			{ desc = "Telescope: Gitの変更ファイルを一覧表示" }
-		)
-		-- Gitのログ一覧を表示
-		vim.keymap.set("n", "<leader>gl", builtin.git_commits, { desc = "Telescope: Gitのログ一覧を表示" })
-		-- 編集履歴を一覧表示
-		vim.keymap.set("n", "<leader>fo", builtin.oldfiles, { desc = "Telescope: 編集履歴を一覧表示" })
-		-- バッファーの一覧表示
-		vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope: バッファーの一覧表示" })
-		-- ヘルプタグの検索
-		vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope: ヘルプタグの検索" })
-		-- カラーテーマの一覧表示
-		vim.keymap.set(
-			"n",
-			"<leader>fc",
-			builtin.colorscheme,
-			{ desc = "Telescope: カラーテーマの一覧表示" }
-		)
-		-- Vimオプションの表示
-		vim.keymap.set("n", "<leader>fv", builtin.vim_options, { desc = "Telescope: Vimオプションの表示" })
-		-- キーマップの一覧表示
-		vim.keymap.set("n", "<leader>fk", builtin.keymaps, { desc = "Telescope: キーマップの一覧表示" })
-
-		-- 通知の一覧
-		vim.api.nvim_set_keymap(
-			"n",
-			"<leader>fn",
-			":Telescope notify<CR>",
-			{ noremap = true, silent = true, desc = "Telescope: 通知の一覧" }
-		)
-
-		-- ファイルブラウザ
-		vim.api.nvim_set_keymap("n", "<leader>fbb", ":Telescope file_browser path=$:p:h select_buffer=true<CR>", {
-			noremap = true,
-			desc = "Telescope: ツリー形式でファイルを検索",
-		})
-
-		-- よく使用するファイルの検索
-		vim.keymap.set(
-			"n",
-			"<leader>ffr",
-			"<Cmd>Telescope frecency workspace=CWD<CR>",
-			{ desc = "Telescope: よく使用するファイルを表示" }
-		)
+		-- Telescopeの拡張機能（notify, frecency, file_browser等）は
+		-- プラグインが読み込まれたタイミングでロードする必要があるため、ここでロードする
+		-- エラーを防ぐため pcall でラップするのが安全
+		pcall(require("telescope").load_extension, "notify")
+		pcall(require("telescope").load_extension, "file_browser")
+		pcall(require("telescope").load_extension, "frecency")
 	end,
 }
