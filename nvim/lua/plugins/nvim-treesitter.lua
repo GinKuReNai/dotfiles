843072d29d2308c4c965adf36fc39252f47bf989
GinKuReNai <akito121215@gmail.com>
2026-02-25 09:25:32 +0900

    refactor :recycle: treesitterの設定を綺麗に

--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -1,16 +1,21 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
-	build = function()
-		require("nvim-treesitter.install").update({ with_sync = true })
-	end,
-	config = function()
-		require("nvim-treesitter.configs").setup({
-			highlight = {
-				enable = true, -- シンタックスハイライトを有効化
-				additional_vim_regex_highlighting = false,
-			},
-			indent = { enable = true }, -- インデントの自動調整
-		})
-	end,
-	event = { "BufReadPost", "BufNewFile" }, -- ファイルを開いたときに読み込む
+	build = ":TSUpdate",
+	event = { "BufReadPost", "BufNewFile" },
+	main = "nvim-treesitter.configs", -- `require("nvim-treesitter.configs").setup()` を自動実行させるための設定
+	opts = {
+		-- 必須またはよく使うパーサーを明記
+		ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
+
+		-- 未インストールのファイルタイプを開いた際に自動でパーサーをインストール
+		auto_install = true,
+
+		highlight = {
+			enable = true, -- シンタックスハイライトを有効化
+			additional_vim_regex_highlighting = false,
+		},
+		indent = {
+			enable = true,
+		}, -- インデントの自動調整
+	},
 }
