419a3fdcda39af909e1cdf29877eb169219f4850
GinKuReNai <akito121215@gmail.com>
2026-02-25 09:31:54 +0900

    update :arrow_up: よく使うパーサーに追加

--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -5,7 +5,22 @@ return {
 	main = "nvim-treesitter.configs", -- `require("nvim-treesitter.configs").setup()` を自動実行させるための設定
 	opts = {
 		-- 必須またはよく使うパーサーを明記
-		ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
+		ensure_installed = {
+			"c",
+			"lua",
+			"vim",
+			"vimdoc",
+			"query",
+			"markdown",
+			"markdown_inline",
+			"python",
+			"go",
+			"javascript",
+			"nginx",
+			"dockerfile",
+			"typescript",
+			"php",
+		},
 
 		-- 未インストールのファイルタイプを開いた際に自動でパーサーをインストール
 		auto_install = true,
